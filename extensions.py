ed1bcb61aa3aad5e829a4147cb899e13b5c0c6d8
Vinh Tu <vinhtu@gmail.com>
2017-09-24 18:15:57 +0100

    refactor and bug fix

--- extensions.py
+++ extensions.py
@@ -8,26 +8,30 @@ python extensions.py
 
 import json
 
-res = []
+def generate_extensions():
+    res = []
 
-with open("Extensions.source") as r:
-    for line in r:
-        line = line.strip()
-        parts = line.split(":")
-        print(parts)
-        if len(parts)==2:
-            try:
-                reps = int(parts[1])
-            except:
-                print("Can't parse number: %s" % parts[1])
+    with open("Extensions.source") as r:
+        for line in r:
+            line = line.strip()
+            parts = line.split(":")
+            print(parts)
+            if len(parts)==2:
+                try:
+                    reps = int(parts[1])
+                except:
+                    print("Can't parse number: %s" % parts[1])
+                    reps = 1
+            else:
                 reps = 1
-        else:
-            reps = 1
 
-        for i in range(reps):
-            res.append(parts[0])
+            for i in range(reps):
+                res.append(parts[0])
 
-with open("extensions.json", "w") as w:
-    json.dump(dict(extensions=res), w, sort_keys=True)
+    print("Generated extensions.json")
+    return res
 
-print("Generated extensions.json")
+if __name__ == '__main__':
+    res = generate_extensions()
+    with open("extensions.json", "w") as w:
+        json.dump(dict(extension=res), w, sort_keys=True)
