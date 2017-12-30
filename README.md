2094c6fe00a4ff7444142c7e5645fab8e522dce7
Vinh Tu <vinhtu@gmail.com>
2017-12-30 15:04:12 +0000

    filenames.py now dumps prefixes into prefixes.json

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # filesonmy386
 Tracery code for the Twitter bot known as @FilesOnMy386
+
+## TODO
+We need better documentation!
--- filenames.py
+++ filenames.py
@@ -1,19 +1,35 @@
+"""
+1. This parses the corpus files under corpus/in_all
+2. ...
+3. It dumps the prefixes into prefixes.json
+
+"""
+
+
 import sys
 import json
 import os
+from collections import defaultdict
 
 IN_DIR = "corpus/in_all"
 
+
+
 def get_filenames():
-    res = []
+    res = defaultdict(list)
     filenames = os.listdir(IN_DIR)
 
     for fn in filenames:
-        res = res + dofile(os.path.join(IN_DIR, fn))
+        print("processing:", fn)
+        newres = dofile(os.path.join(IN_DIR, fn))
+        for (k,v) in newres.items():
+            res[k] += v
+    print("processed %d files" % len(filenames))
     return res
 
 def dofile(infile):
     lst = []
+    prefixes = set()
     with open(infile) as r:
         for l in r:
             for word in l.split(","):
@@ -25,15 +41,26 @@ def dofile(infile):
                     prefix, suffix = word.split('.')
                     # print prefix, suffix
                     lst.append("%s.%s" % (prefix, suffix))
+                    prefixes.add(prefix)
                 except:
                     # print "Error:", word
                     pass
-    return lst
+    return {'filenames': lst,
+            'prefixes': sorted(list(prefixes))}
+
 
 def main(in_dir=None):
-    n = get_filenames()
-    print(n)
-    print(len(n))
+    result = get_filenames()
+    n = result["filenames"]
+    prefixes = result["prefixes"]
+    # print("filenames", n)
+    print("count:", len(n))
+    print("prefix count:", len(prefixes))
+
+    with open("prefixes.json", "w") as w:
+        json.dump(dict(prefixes=prefixes), w)
+
+    print("Done. HAve a nice Day!")
 
 if __name__=="__main__":
     #main(sys.argv[1])
